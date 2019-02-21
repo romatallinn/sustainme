d606d369ddfc13675ad9c01ad2dd442705157637
Matthijs <M.Arnoldus-1@student.tudelft.nl>
2019-02-21 17:36:28 +0100

    Added three buttons, with shutdown functionality

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,8 @@
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void close(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
--- src/main/java/View.java
+++ src/main/java/View.java
@@ -4,13 +4,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+
 public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("application.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
