b8061b3af891e1b01dafaefc5acacaffda3c0012
Roderick <Zero-Smaus@users.noreply.github.com>
2019-02-25 11:35:50 +0100

    Added comments to code

--- src/main/java/Model/UserProfile.java
+++ src/main/java/Model/UserProfile.java
@@ -12,7 +12,12 @@ class UserProfile {
     private int level;
     private int experience;
 
-
+    /**
+     * Constructor for the UserProfile class, defaults level to 1, and experience to 0
+     * @param first
+     * @param last
+     * @param email
+     */
     UserProfile(String first, String last, String email){
         firstName = first;
         lastName = last;
@@ -21,18 +26,25 @@ class UserProfile {
         experience = 0;
     }
 
+    /**
+     * Increases score, calls CheckLevel
+     * @param score
+     */
     void IncreaseScore(int score){
         experience += score;
         this.CheckLevel();
     }
 
+    /**
+     * Checks current experience to see if the user shouldlevel up
+     */
     private void CheckLevel(){
         while (experience >= 10*pow(2,level-1)) {
             experience -= 10 * pow(2, level - 1);
             level++;
         }
     }
-
+    
     String getFirstName(){
         return firstName;
     }
