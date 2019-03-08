14c45fa3ef9380b1993c0ef5875e67ce786ecc7a
Roman Sirokov <r.sirokov@student.tudelft.nl>
2019-03-08 16:39:16 +0100

    JavaFX Application Structure Change
    The commit introduces a new JavaFxApplication class.
    
    It handles the instantiation of the actual JavaFX application/view.
    
    This functionality was previously handled in the ApplicationEntry,
    but due to its view nature, it was moved to the view package into
    a separate class.

--- src/main/java/supporting/ApplicationEntry.java
+++ src/main/java/supporting/ApplicationEntry.java
@@ -1,20 +1,10 @@
 package supporting;
 
-import controller.SignUpController;
-
-import javafx.application.Application;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-import view.implementation.javafx.JavaFxSignUpView;
-import view.interfaces.ISignUpView;
+import view.implementation.javafx.JavaFxApplication;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,7 +13,7 @@ import java.util.List;
 /**
  * The entry point of the whole application; the very first class to be called.
  */
-public class ApplicationEntry extends Application {
+public class ApplicationEntry {
 
     /**
      * Entry point of the application.
@@ -39,32 +29,9 @@ public class ApplicationEntry extends Application {
         // Indicate that the application is setup and running.
         System.out.println("Application is running...\n\n");
 
-        launch(args);
+        JavaFxApplication.launchApp(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        // Instantiate the initial controller of the application
-        ISignUpView view = new JavaFxSignUpView();
-        SignUpController controller = new SignUpController(view);
-
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/JavaFXLoginView.fxml"));
-
-        view.initView(controller);
-        loader.setController(view);
-
-        Parent root = (Parent)loader.load();
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("/css/Login.css").toString());
-
-        primaryStage.setTitle("SustainMe - Login");
-        primaryStage.setScene(scene);
-        primaryStage.setMaximized(true);
-        primaryStage.show();
-    }
 
 
     /**
---
+++ src/main/java/view/implementation/javafx/JavaFxApplication.java
@@ -0,0 +1,43 @@
+package view.implementation.javafx;
+
+import controller.SignUpController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import view.interfaces.ISignUpView;
+
+public class JavaFxApplication extends Application {
+
+    public static void launchApp(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Instantiate the initial controller of the application
+        ISignUpView view = new JavaFxSignUpView();
+        SignUpController controller = new SignUpController(view);
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/JavaFXLoginView.fxml"));
+
+        view.initView(controller);
+        loader.setController(view);
+
+        Parent root = (Parent)loader.load();
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/css/Login.css").toString());
+
+        primaryStage.setTitle("SustainMe - Login");
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+}
