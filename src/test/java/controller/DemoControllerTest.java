1f61e28a386abe930c1ddabcc966758f2b521c19
Roderick <r.m.debrittoheemskerk@student.tudelft.nl>
2019-03-17 22:50:48 +0100

    Fix separation of controller, view, model

--- src/test/java/controller/DemoControllerTest.java
+++ src/test/java/controller/DemoControllerTest.java
@@ -1,9 +1,6 @@
-package server;
+package controller;
 
-import controller.DemoController;
-import controller.Greeting;
-import org.springframework.context.annotation.ComponentScan;
-import server.Application;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import server.Application;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = Application.class, webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class DemoControllerTest {
 
     @Test
