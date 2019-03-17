e0d1467569021ba185e8c7e1cc795ec0ec2317f3
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-03-17 23:43:44 +0100

    Partial implementation of fractal tree
    jogamp.org offline so no documentation for now

--- src/main/java/view/implementation/javafx/JavaFxFractalTreeView.java
+++ src/main/java/view/implementation/javafx/JavaFxFractalTreeView.java
@@ -1,11 +1,16 @@
 package view.implementation.javafx;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
+import view.element.FractalTree;
+
+import java.awt.image.BufferedImage;
 
 
 public class JavaFxFractalTreeView extends Application {
@@ -13,11 +18,16 @@ public class JavaFxFractalTreeView extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        BufferedImage fractalTree = FractalTree.makeImage(300, 200, new int[] {3, 90}, "nadyne");
+        WritableImage fractalTreeImage = SwingFXUtils.toFXImage(fractalTree, null);
+
         // Creates the canvas
         Group root = new Group();
         Canvas canvas = new Canvas(300, 250);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.drawImage(fractalTreeImage, 0, 0);
         root.getChildren().add(canvas);
+
 
         // Instantiate the view
         primaryStage.setTitle("SustainMe - Fractal Tree");
@@ -25,5 +35,8 @@ public class JavaFxFractalTreeView extends Application {
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
+
+
+
 
 }
