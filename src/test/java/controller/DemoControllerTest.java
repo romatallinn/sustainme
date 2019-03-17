7582363d25470eb837939290be3d3e031075b268
Matthijs <M.Arnoldus-1@student.tudelft.nl>
2019-03-17 22:16:49 +0100

    no message

--- src/test/java/controller/DemoControllerTest.java
+++ src/test/java/controller/DemoControllerTest.java
@@ -1,4 +1,8 @@
 package controller;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class DemoControllerTest {
 }
