8b58e25586e177850d1389f65f49c1ce296d2385
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-03-17 19:21:29 +0100

    Remove tests that can not run headless

--- src/test/java/view/FractalTreeTest.java
+++ src/test/java/view/FractalTreeTest.java
@@ -36,31 +36,4 @@ public class FractalTreeTest {
         FractalTree testTree = new FractalTree(new int[] {}, "nadyne");
         assertEquals(testTree.seedFromName(), 110 + 2 * 97 + 3 * 100 + 4 * 121 + 5 * 110 + 6 * 101);
     }
-
-    @Test
-    public void generatedImageWidth() {
-        /**
-         * NOTE: Will warn on MacOS Mojave because of a JVM bug:
-         * WARNING: NSWindow drag regions should only be invalidated on the Main Thread!
-         */
-        assertEquals(FractalTree.makeImage(200, 300, new int[] {}, "test").getWidth(), 200);
-    }
-
-    @Test
-    public void generatedImageHeight() {
-        /**
-         * NOTE: Will warn on MacOS Mojave because of a JVM bug:
-         * WARNING: NSWindow drag regions should only be invalidated on the Main Thread!
-         */
-        assertEquals(FractalTree.makeImage(200, 300, new int[] {}, "test").getHeight(), 300);
-    }
-
-    @Test
-    public void generatedImagePixelCenter() {
-        /**
-         * NOTE: Will warn on MacOS Mojave because of a JVM bug:
-         * WARNING: NSWindow drag regions should only be invalidated on the Main Thread!
-         */
-        assertEquals(FractalTree.makeImage(1, 1, new int[] {}, "test").getRGB(0, 0), 0);
-    }
 }
