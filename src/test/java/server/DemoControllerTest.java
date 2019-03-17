7d0a9ebd5aebad3b7951f3b3c3ec686b3393c9c6
Matthijs <M.Arnoldus-1@student.tudelft.nl>
2019-03-17 22:38:19 +0100

    Demo controller test

---
+++ src/test/java/server/DemoControllerTest.java
@@ -0,0 +1,29 @@
+package server;
+
+import controller.DemoController;
+import controller.Greeting;
+import org.springframework.context.annotation.ComponentScan;
+import server.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class DemoControllerTest {
+
+    @Test
+    public void getGreetingTest() {
+        Greeting greeting = DemoController.getGreeting("Henk");
+        
+    }
+
+
+}
