5dc7d3775e60dab459850af65b1fa534de602e5f
Roderick <r.m.debrittoheemskerk@student.tudelft.nl>
2019-03-24 18:19:58 +0100

    Test database interaction fixed

--- src/test/java/server/ServerControllerTest.java
+++ src/test/java/server/ServerControllerTest.java
@@ -4,21 +4,12 @@ import model.objects.InitRequest;
 import model.objects.UserData;
 import model.objects.VegetarianRequest;
 import model.objects.VegetarianResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import supporting.ServerApi;
 import server.supporting.FirebaseConnection;
 
 import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 
 public class ServerControllerTest {
@@ -27,19 +18,15 @@ public class ServerControllerTest {
     public void setup() {
         FirebaseConnection.initApp();
     }
-
-
-
-
-
+    ServerController serve = new ServerController();
 
     @Test
     public void retrieve_user_data_Test() throws InterruptedException {
-        UserData userData =  new ServerController().retrieve_user_data("5An25iOCs5bisQ2ORzaaoUD9nNo2");
+        UserData userData =  serve.retrieve_user_data("staticTestUser");
 
-        assertEquals("5An25iOCs5bisQ2ORzaaoUD9nNo2", userData.uid);
-        assertEquals("Firstname", userData.fname);
-        assertEquals("Lastname", userData.lname);
+        assertEquals("staticTestUser", userData.uid);
+        assertEquals("Static", userData.fname);
+        assertEquals("Testuser", userData.lname);
         assertNotNull(userData.level);
         assertNotNull(userData.experience);
         assertNotNull(userData.vegmeals);
@@ -47,16 +34,16 @@ public class ServerControllerTest {
 
     @Test
     public void vegetarianMealTest() throws InterruptedException {
-        UserData userData =  new ServerController().retrieve_user_data("5An25iOCs5bisQ2ORzaaoUD9nNo2");
-        VegetarianRequest vegetarianRequest = new VegetarianRequest("5An25iOCs5bisQ2ORzaaoUD9nNo2", 2);
-        VegetarianResponse vegetarianResponse =  new ServerController().vegetarianMeal(vegetarianRequest);
-        UserData after =  new ServerController().retrieve_user_data("5An25iOCs5bisQ2ORzaaoUD9nNo2");
+        UserData userData =  serve.retrieve_user_data("dynamicTestUser");
+        VegetarianRequest vegetarianRequest = new VegetarianRequest("dynamicTestUser", 2);
+        VegetarianResponse vegetarianResponse =  serve.vegetarianMeal(vegetarianRequest);
+        UserData after =  serve.retrieve_user_data("dynamicTestUser");
         assertEquals(10,after.experience - userData.experience);
     }
 
     @Test
     public void initUserTest() {
-        InitRequest initRequest = new InitRequest("Test", "Firstname","Lastname");
+        InitRequest initRequest = new InitRequest("initTestUser", "Firstname","Lastname");
         ResponseEntity responseEntity = new ServerController().initUser(initRequest);
         assertNotNull(responseEntity);
     }
