0a67859c46d18193fcd25b970bbe6f5f41a1f92f
Matthijs <M.Arnoldus-1@student.tudelft.nl>
2019-03-25 11:57:15 +0100

    Update Home View Style

--- src/main/java/view/implementation/javafx/JavaFxFoodView.java
+++ src/main/java/view/implementation/javafx/JavaFxFoodView.java
@@ -4,6 +4,7 @@ import controller.FoodController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import view.interfaces.IFoodView;
 
--- src/main/java/view/implementation/javafx/JavaFxHomeView.java
+++ src/main/java/view/implementation/javafx/JavaFxHomeView.java
@@ -4,6 +4,9 @@ import controller.HomescreenController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import view.interfaces.IHomeView;
 
@@ -33,6 +36,8 @@ public class JavaFxHomeView extends JavaFxView implements IHomeView {
     private Button score;
     @FXML
     private ProgressBar expBar;
+    @FXML
+    private Circle profilePic;
 
 
     /**
@@ -57,6 +62,8 @@ public class JavaFxHomeView extends JavaFxView implements IHomeView {
     @Override
     protected void updateLabels() {
         controller.updateViewWithData();
+        Image im = new Image("https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png", false);
+        profilePic.setFill(new ImagePattern(im));
     }
 
     @Override
