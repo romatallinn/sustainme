1ee42ffa3ac07ab823fe25c98f6185105eb302f2
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-03-26 23:36:36 +0100

    Add tests for fade method and degree method of fractalTree class

--- src/test/java/view/FractalTreeTest.java
+++ src/test/java/view/FractalTreeTest.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.scene.paint.Color;
 import view.element.FractalTree;
 import org.junit.*;
 
@@ -36,4 +37,72 @@ public class FractalTreeTest {
         FractalTree testTree = new FractalTree("nadyne");
         assertEquals(testTree.seedAsLong(), 110 + 2 * 97 + 3 * 100 + 4 * 121 + 5 * 110 + 6 * 101);
     }
+
+    @Test
+    public void deg() {
+        FractalTree testTree = new FractalTree("nadyne");
+        assertEquals(testTree.deg(93), 93, 0);
+    }
+
+    @Test
+    public void degMin() {
+        FractalTree testTree = new FractalTree("nadyne");
+        assertEquals(testTree.deg(-23), -23 + 360, 0);
+    }
+
+    @Test
+    public void degMax() {
+        FractalTree testTree = new FractalTree("nadyne");
+        assertEquals(testTree.deg(379), 379 - 360, 0);
+    }
+
+    @Test
+    public void degNull() {
+        FractalTree testTree = new FractalTree("nadyne");
+        assertEquals(testTree.deg(0), 0, 0);
+    }
+
+    @Test
+    public void degFull() {
+        FractalTree testTree = new FractalTree("nadyne");
+        assertEquals(testTree.deg(360), 360 - 360, 0);
+    }
+
+    @Test
+    public void fadeBlue() {
+        FractalTree testTree = new FractalTree("nadyne");
+        Color faded = testTree.fade(
+            new Color(0.3, 0.8, 0.3, 0.9), new Color(0.8, 0.2, 0.5, 0.3));
+        assertEquals(faded.getBlue(), 0.33000001311302185, 0);
+
+    }
+
+    @Test
+    public void fadeRed() {
+        FractalTree testTree = new FractalTree("nadyne");
+        Color faded = testTree.fade(
+            new Color(0.3, 0.8, 0.3, 0.9), new Color(0.8, 0.2, 0.5, 0.3) );
+        assertEquals(faded.getRed(), 0.375, 0);
+
+    }
+
+    @Test
+    public void fadeGreen() {
+        FractalTree testTree = new FractalTree("nadyne");
+        Color faded = testTree.fade(
+            new Color(0.3, 0.8, 0.3, 0.9), new Color(0.8, 0.2, 0.5, 0.3) );
+        assertEquals(faded.getGreen(), 0.7100000381469727, 0);
+
+    }
+
+    @Test
+    public void fadeOpacity() {
+        FractalTree testTree = new FractalTree("nadyne");
+        Color faded = testTree.fade(
+            new Color(0.3, 0.8, 0.3, 0.9), new Color(0.8, 0.2, 0.5, 0.3) );
+        assertEquals(faded.getOpacity(), 0.8100000023841858, 0);
+
+    }
+
 }
+
