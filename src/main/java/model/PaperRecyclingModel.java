af87158c8008f3ca3315c64d2c3c67a1a981b1b6
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-04-03 20:11:42 +0200

    Add class PaperRecyclingModel to model

---
+++ src/main/java/model/PaperRecyclingModel.java
@@ -0,0 +1,65 @@
+package model;
+
+import model.objects.PaperRecyclingRequest;
+import model.objects.PaperRecyclingResponse;
+import org.springframework.web.client.RestTemplate;
+import supporting.ServerApi;
+
+import javax.jws.soap.SOAPBinding;
+
+public class PaperRecyclingModel {
+
+    private double paperRecyclingCount = -1;
+
+    /**
+     * Initializes the PaperRecycling feature of the user profile.
+     */
+    public void init() {
+
+        if (UserProfile.getInstance().authToken.isEmpty()) {
+            paperRecyclingCount = 0;
+            return;
+        }
+
+        addAmountPaperRecycling(0);
+    }
+
+    /**
+     * Returns the total amount of recycled paper.
+     * @return - the amount of recycled paper
+     */
+    public double getPaperRecyclingCount() {
+
+        if (paperRecyclingCount < 0) {
+            this.init();
+        }
+
+        return paperRecyclingCount;
+    }
+
+    /**
+     * Invokes request to server, notifying it about the action of amount of recycled paper.
+     * @param - weight of recycled paper in kg
+     */
+    private void addAmountPaperRecycling(double kg) {
+
+        if (UserProfile.getInstance().authToken.isEmpty()) {
+            paperRecyclingCount = 0;
+            return;
+        }
+
+        final String uri = ServerApi.HOST + ServerApi.PAPER_RECYCLING;
+
+        PaperRecyclingRequest paperRecyclingRequest =
+            new PaperRecyclingRequest(UserProfile.getInstance().getUid(), kg);
+
+        RestTemplate restTemplate = new RestTemplate();
+        PaperRecyclingResponse result =
+            restTemplate.postForObject(uri, paperRecyclingRequest, PaperRecyclingResponse.class);
+
+        UserProfile.getInstance().setLocalExp(result.getExperience());
+        UserProfile.getInstance().setLocalCo2Stats(result.getCo2Reduced());
+
+        paperRecyclingCount = result.getAmount();
+    }
+}
