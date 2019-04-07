906d603582df8bee393a47d152921d20b1d77bf1
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-04-07 15:00:48 +0200

    Add fractalTreeController to controller

---
+++ src/main/java/controller/FractalTreeController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.FractalTreeModel;
+
+public class FractalTreeController {
+
+    private FractalTreeModel model;
+
+    public FractalTreeController(FractalTreeModel model) {
+        this.model = model;
+    }
+}
