eff52ca178f44fa7fa992017348c9f5e84df43ee
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-04-07 15:33:44 +0200

    Delete FractalTreeController, add scores fractalTree

--- src/main/java/view/implementation/javafx/JavaFxFractalTreeView.java
+++ src/main/java/view/implementation/javafx/JavaFxFractalTreeView.java
@@ -7,9 +7,12 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import model.FractalTreeModel;
 import model.UserProfile;
 
 
+import model.objects.FractalTreeRequest;
+import model.objects.FractalTreeResponse;
 import view.element.FractalTree;
 
 
@@ -33,20 +36,29 @@ public class JavaFxFractalTreeView extends Application {
         gc.fillRect(600, 0, 200, 800);
 
         String firstName = UserProfile.getInstance().getFirstName();
+        FractalTreeResponse result = new FractalTreeModel().fractalTreeGetData();
+        double bikeCO2 = result.getBikeCo2();
+        double vegmealsCO2 = result.getVegmealsCO2();
+        double localproduceCO2 = result.getLocalproduceCO2();
+        double publicCO2 = result.getPublicCO2();
+        // TO DO: temp, sol, paper, plastic
 
 
         // Scores: bike-fuchsia, localProduce-blueviolet, publicTransport-azure,
-        // veggie-lime, temperature-darkorange, solarPanels-red
+        // veggie-lime, temperature-darkorange, solarPanels-red, paper-yellow, plastic-gray
         FractalTree ft = new FractalTree(
                 firstName,
-                new double[]{30, 39, 20, 40, 450, 500},
+                new double[]{bikeCO2, localproduceCO2, publicCO2, vegmealsCO2, 450, 500, 889, 909},
                 new Color[]{
                     Color.FUCHSIA,
                     Color.BLUEVIOLET,
                     Color.AZURE,
                     Color.LIME,
                     Color.DARKORANGE,
-                    Color.RED}
+                    Color.RED,
+                    Color.YELLOW,
+                    Color.GRAY
+                    }
         );
         ft.drawTree(canvas);
         Group root = new Group();
