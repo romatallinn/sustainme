eabba6f5bfc0adc30d86754a627b9dba963722e4
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-04-07 18:01:40 +0200

    Add methods to badgesModel

--- src/main/java/model/BadgesModel.java
+++ src/main/java/model/BadgesModel.java
@@ -1,4 +1,37 @@
 package model;
 
 public class BadgesModel {
+
+    public void vegetarianMealBadge(){
+
+        int count = new FoodModel().getVegMealsCount();
+
+        if (count = 10){
+
+        }
+    }
+
+    public void temperatureBadge(){
+
+    }
+
+    public void kmNoCarUsedBadge(){
+
+    }
+    
+    public void distanceByBikeBadge(){
+
+    }
+
+    public void co2ReducedBadge(){
+
+    }
+
+    public void levelHundredBadge(){
+
+    }
+
+    public void distanceByTrainBadge(){
+
+    }
 }
