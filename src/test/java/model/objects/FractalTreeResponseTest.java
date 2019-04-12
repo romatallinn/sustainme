aeda03814f99f3672564fbee13a1625ece9bbfb6
Nadyne Aretz <n.l.aretz@student.tudelft.nl>
2019-04-12 19:13:24 +0200

    Fix tests fractalTreeResponse

--- src/test/java/model/objects/FractalTreeResponseTest.java
+++ src/test/java/model/objects/FractalTreeResponseTest.java
@@ -8,25 +8,38 @@ public class FractalTreeResponseTest {
 
     @Test
     public void getPublicCo2() {
-        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4);
+        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4, 8, 9);
         assertEquals(fractalTreeResponse.getPublicCO2(), 3.4, 1);
     }
 
     @Test
     public void getVegmealsCo2() {
-        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4);
+        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4, 8, 9);
         assertEquals(fractalTreeResponse.getVegmealsCO2(), 2.5, 1);
     }
 
     @Test
     public void getLocalproduceCo2() {
-        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4);
+        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4, 8, 9);
         assertEquals(fractalTreeResponse.getLocalproduceCO2(), 7, 1);
     }
 
     @Test
     public void getBikeCo2() {
-        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4);
+        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4, 8, 9);
         assertEquals(fractalTreeResponse.getBikeCo2(), 1, 1);
     }
+
+    @Test
+    public void getPlasticrecyclingCO2() {
+        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4, 8, 9);
+        assertEquals(fractalTreeResponse.getPlasticrecyclingCO2(), 8, 1);
+    }
+
+    @Test
+    public void getPaperrecyclingCO2() {
+        FractalTreeResponse fractalTreeResponse = new FractalTreeResponse(1, 2.5, 7, 3.4, 8, 9);
+        assertEquals(fractalTreeResponse.getPaperrecyclingCO2(), 9, 1);
+    }
+
 }
